a1374caa9ec0ce43df76892b9e6964750ae1d4c3
uk onyi <georgegoldman2014@gmail.com>
2020-05-23 14:09:10 +0100

    updt

--- models/farm.js
+++ models/farm.js
@@ -3,9 +3,7 @@ module.exports = (sequelize, DataTypes) => {
     const Farm = sequelize.define('Farm', {
         name: DataTypes.TEXT,
         type: DataTypes.TEXT,
-        produce: DataTypes.TEXT,
-        amount: DataTypes.INTEGER,
-        faming: DataTypes.TEXT
+        produce: DataTypes.TEXT
     }, {});
     Farm.associate = function(models) {
         // associations can be defined here
